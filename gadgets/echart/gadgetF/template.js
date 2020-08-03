8d307ba38cd46c4c23eee0f9bd0236e1aee8dee5
dsanteodoro <dsanteodoro@minsait.com>
2020-08-03 09:35:58 +0200

    añadido grupo a gadgets

---
+++ gadgets/echart/gadgetF/template.js
@@ -0,0 +1,5 @@
+//Write your controller (JS code) code here\r\n\r\n
+//Focus here and F11 to full screen editor\r\n\r\n
+//This function will be call once to init components\r\nvm.initLiveComponent = function(){\r\n\r\n};\r\n\r\n
+//This function will be call when data change. On first execution oldData will be null\r\nvm.drawLiveComponent = function(newData, oldData){\r\n\r\n};\r\n\r\n
+//This function will be call on element resize\r\nvm.resizeEvent = function(){\r\n\r\n}\r\n\r\n//This function will be call when element is destroyed\r\nvm.destroyLiveComponent = function(){\r\n\r\n};
